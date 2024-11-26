5ab86e286a27499bb028fe850487ddf3a6f2760b
OSUNJIMI IBRAHIM <osunjimibrahim123@gmail.com>
2024-11-26 06:31:40 +0100

    second commit

--- index.js
+++ index.js
@@ -3,123 +3,208 @@ let listOfProducts = [
        productImage: './assets/images/image-waffle-desktop.jpg' ,
        productName: 'Waffle',
        productDescription: 'Waffle with Berries',
-       productPrice: 6.50
+       productPrice: 6.50,
+       productQuantity: 1
     },
     {
         productImage: './assets/images/image-creme-brulee-desktop.jpg' ,
         productName: 'Crème Brûlée',
         productDescription: 'Vanilla Bean Crème Brûlée',
-        productPrice: 7.00
+        productPrice: 7.00,
+       productQuantity: 1
      },
      {
         productImage: './assets/images/image-macaron-tablet.jpg' ,
         productName: 'Macaron',
         productDescription: 'Macaron Mix of Five',
-        productPrice: 8.00
+        productPrice: 8.00,
+       productQuantity: 1
      },
      {
         productImage: './assets/images/image-tiramisu-desktop.jpg' ,
         productName: 'Tiramisu',
         productDescription: 'Classic Tiramisu',
-        productPrice: 5.50
+        productPrice: 5.50,
+       productQuantity: 1
      },
      {
         productImage: './assets/images/image-baklava-tablet.jpg' ,
         productName: 'Baklava',
         productDescription: 'Pistachio Baklava',
-        productPrice: 4.00
+        productPrice: 4.00,
+       productQuantity: 1
      },
      {
         productImage: './assets/images/image-meringue-desktop.jpg' ,
         productName: 'Pie',
         productDescription: 'Lemon Meringue Pie',
-        productPrice: 5.00
+        productPrice: 5.00,
+       productQuantity: 1
      },
      {
         productImage: './assets/images/image-cake-tablet.jpg' ,
         productName: 'Pie',
         productDescription: 'Lemon Meringue Pie',
-        productPrice: 5.00
+        productPrice: 5.00,
+       productQuantity: 1
      },
      {
         productImage: './assets/images/image-brownie-tablet.jpg' ,
         productName: 'Pie',
         productDescription: 'Lemon Meringue Pie',
-        productPrice: 5.00
+        productPrice: 5.00,
+       productQuantity: 1
      },
      {
         productImage: './assets/images/image-panna-cotta-tablet.jpg' ,
         productName: 'Pie',
         productDescription: 'Lemon Meringue Pie',
-        productPrice: 5.00
+        productPrice: 5.00,
+       productQuantity: 1
      }
 ]
 
-
-
-const Products = document.getElementById('products')
+const Products = document.getElementById('products');
 listOfProducts.forEach((element, index) => {
     Products.innerHTML += `
-     <div data-aos="zoom-in-up" class="product col-lg-4 col-md-6 col-sm">
-        <div class="product-image position-relative">
-          <div class="img"><img src="${element.productImage}" alt="" width="100%" class="rounded-3"></div>
-          <button type="button" class="btn btn-light rounded-5 dtc-btn position-absolute px-4 d-flex align-items-center justify-content-center gap-2" id="addToCart" onclick ="placeOrder(${index})"><i class="fa-solid fa-cart-plus"></i>Add to Cart</button>
+        <div class="product col-lg-4 col-md-6 col-sm"> 
+            <div class="product-image position-relative">
+                <div class="img">
+                    <img src="${element.productImage}" alt="" width="100%" class="rounded-3">
+                </div>
+                <button 
+                    type="button" 
+                    class="btn btn-light rounded-5 dtc-btn position-absolute px-4 d-flex align-items-center justify-content-center gap-2" 
+                    id="addToCart" 
+                    onclick="showAndHide(${index})">
+                    <i class="fa-solid fa-cart-plus"></i>Add to Cart
+                </button>
+                <div 
+                    class="bg-light rounded-5 dtc-btn position-absolute px-4 d-flex align-items-center justify-content-center gap-3 inc-1" 
+                    id="addToCart2">
+                    <span class="inc pb-1 fs-3 minus">-</span>
+                    <span class="amount">01</span>
+                    <span class="inc pb-1 plus"><b>+</b></span>
+                </div>
+            </div>
+            <div class="description mt-4">
+                <p class="name-of-item">${element.productName}</p>
+                <p><b>${element.productDescription}</b></p>
+                <p class="price"><b>$${element.productPrice}</b></p>
+            </div>
         </div>
-        <div class="description mt-4">
-          <p class="name-of-item">${element.productName}</p>
-          <p><b>${element.productDescription}</b></p>
-          <p class="price"><b>$${element.productPrice}</b></p>
-        </div>
-      </div>
-    `
+    `;
 });
 
-
-
-// let addToCart = document.getElementById('addToCart')
-const cartProduct = []
-
-const placeOrder = (index) =>{
-  document.getElementById("all").style.display="block";
-  document.getElementById("preall").style.display="none";
-   let onOrder = document.getElementById('onOrder')
-   const selectedProduct = listOfProducts[index]
-   cartProduct.push(selectedProduct)
-
-    onOrder.innerHTML += `
-   <div class='d-flex justify-content-between align-items-center'>
-         <div class="left">
-            <h6><b>${selectedProduct.productDescription}</b></h6>
-            <div class="ordered-description d-flex justify-content-betwee align-items-center gap-3">
-              <h6 class="m-0 p-0 ordered-number">1X</h6>
-              <p class="m-0 p-0">@<del>$${(selectedProduct.productPrice * 1.5)}</del></p>
-              <p class="m-0 p-0"><b>$${selectedProduct.productPrice}</b></p>
-            </div>
-          </div>
-          <div class="cancel">
-            <p class="fs-4 p-0 m-0 cancel-icon"><i class="fa-regular fa-circle-xmark"></i></p>
-          </div>
-          </div>
-          <hr>
-    `
-    console.log(cartProduct);
-    console.log(cartProduct.length);
-    
-    document.getElementById('Item-num').innerHTML = cartProduct.length
-
-    localStorage.setItem('cart items', JSON.stringify(cartProduct))
-   // console.log(selectedProduct.productDescription)
-   // console.log(selectedProduct.productPrice)
-   // let savedProduct = JSON.parse(localStorage.getItem('savedProduct')) || [];
-   // savedProduct.push({
-   //    productName : selectedProduct.productDescription,
-   //    productPrice: selectedProduct.productPrice,
-   // })
-   // localStorage.setItem('savedItem', JSON.stringify(savedProduct))
-   // console.log(savedProduct);
+const showAndHide = (index) => {
+   console.log("clicked", index);
    
-    totalProduct(cartProduct)
-}
+   const addToCart2 = document.querySelectorAll('#addToCart2')[index];
+   const addToCart = document.querySelectorAll('#addToCart')[index];
+
+   addToCart2.classList.add("d-flex");
+   addToCart2.style.zIndex = '10';
+   addToCart.style.display = "none";
+
+   increment(index)
+   placeOrder(index)
+};
+
+const increment = (index) => {
+   let a = listOfProducts[index].productQuantity; 
+   let minus = document.querySelectorAll('.minus')[index];
+   let plus = document.querySelectorAll('.plus')[index];
+   let amount = document.querySelectorAll('.amount')[index];
+
+   amount.innerText = a < 10 ? "0" + a : a;
+
+   plus.addEventListener('click', () => {
+       a++;
+       a = a < 10 ? "0" + a : a; 
+       amount.innerText = a; 
+       listOfProducts[index].productQuantity = a;
+
+       // Update the quantity in the cart
+       updateCartQuantity(index, a);
+   });
+
+   minus.addEventListener('click', () => {
+       a--;
+       if (a < 1) a = 1; 
+       a = a < 10 ? "0" + a : a; 
+       amount.innerText = a;
+       listOfProducts[index].productQuantity = a;
+
+       // Update the quantity in the cart
+       updateCartQuantity(index, a);
+   });
+};
+
+const updateCartQuantity = (index, quantity) => {
+   const cartItem = cartProduct.find(product => product.productName === listOfProducts[index].productName);
+   if (cartItem) {
+       cartItem.productQuantity = quantity;
+
+       // Update the DOM
+       const cartQuantity = document.querySelectorAll('#onOrder .ordered-number span')[cartProduct.indexOf(cartItem)];
+       if (cartQuantity) {
+           cartQuantity.textContent = quantity < 10 ? quantity : quantity;
+       }
+
+       // Recalculate total price
+       totalProduct(cartProduct);
+   }
+};
+
+
+const cartProduct = []
+console.log(cartProduct)
+
+
+//This my placeOrder adds a product to the cart and updates the UI
+const placeOrder = (index) => {
+   document.getElementById("all").style.display = "block";
+   document.getElementById("preall").style.display = "none";
+ 
+   const selectedProduct = listOfProducts[index];
+   const existingProduct = cartProduct.find(product => product.productName === selectedProduct.productName);
+ 
+   if (existingProduct) {
+       // Update quantity if already in the cart
+       existingProduct.productQuantity = selectedProduct.productQuantity;
+       updateCartQuantity(index, existingProduct.productQuantity);
+   } else {
+      // selectedProduct.productQuantity = listOfProducts[index].productQuantity;
+       cartProduct.push(selectedProduct);
+       let onOrder = document.getElementById('onOrder');
+       onOrder.innerHTML += `
+       <div class='d-flex justify-content-between align-items-center'>
+          <div class="left">
+             <h6><b>${selectedProduct.productDescription}</b></h6>
+             <div class="ordered-description d-flex justify-content-between align-items-center gap-3">
+               <h6 class="m-0 p-0 ordered-number"><span id="theQuantity">${selectedProduct.productQuantity}</span>X</h6>
+               <p class="m-0 p-0">@<del>$${(selectedProduct.productPrice * 1.5)}</del></p>
+               <p class="m-0 p-0"><b>$${selectedProduct.productPrice}</b></p>
+             </div>
+           </div>
+           <div class="cancel">
+             <p class="fs-4 p-0 m-0 cancel-icon"><i class="fa-regular fa-circle-xmark"></i></p>
+           </div>
+       </div>
+       <hr>
+       `;
+   }
+ 
+   document.getElementById('Item-num').innerHTML = cartProduct.length;
+ 
+   // Save updated cart to localStorage
+   localStorage.setItem('cart items', JSON.stringify(cartProduct));
+ 
+   // Recalculate total price
+   totalProduct(cartProduct);
+ };
+ 
+
 // let arrayOfPrice = []
 const totalProduct = (arrayOfPrice) =>{
    let totalPrice = 0;
@@ -157,7 +242,7 @@ let retrieve = () =>{
                <div class="left">
                   <h6><b>${element.productDescription}</b></h6>
                   <div class="ordered-description d-flex justify-content-between align-items-center gap-3">
-                    <h6 class="m-0 p-0 ordered-number">1X</h6>
+                    <h6 class="m-0 p-0 ordered-number">${element.productQuantity}X</h6>
                     <p class="m-0 p-0">@<del>$${(element.productPrice * 1.5)}</del></p>
                     <p class="m-0 p-0"><b>$${element.productPrice}</b></p>
                   </div>
